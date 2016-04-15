14c058862163d7cc68593369174858ee682c733a
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-15 10:08:00 +0300

    Lecture-2
    Basic files

---
+++ Section-2/Lecture-2/Lecture-2 WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  Lecture-2 WatchKit Extension
+//
+//  Created by Pavlo Cretsu on 4/15/16.
+//  Copyright © 2016 Pasha Cretsu. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
