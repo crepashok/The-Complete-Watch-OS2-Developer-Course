afac3dc3915bcb3835597628fedb156a6a5376d5
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-19 09:12:24 +0300

    Lecture-63
    Adding in the arrays

--- Section-7/Standard Dice Game/Standard Dice Game WatchKit Extension/InterfaceController.swift
+++ Section-7/Standard Dice Game/Standard Dice Game WatchKit Extension/InterfaceController.swift
@@ -17,6 +17,9 @@ class InterfaceController: WKInterfaceController {
     
     @IBOutlet var btnDiceSelect: WKInterfaceButton!
     
+    var diceNumberArray = [2, 4, 6, 8, 10, 12, 20, 50, 100]
+    var result : Int = 0
+    
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
