77fddeee1bc8e283f38d0711c4a3cd0a2b853a02
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-19 09:27:57 +0300

    Lecture-68
    Testing the app and fixing the bugs

--- Section-7/Standard Dice Game/Standard Dice Game WatchKit Extension/InterfaceController.swift
+++ Section-7/Standard Dice Game/Standard Dice Game WatchKit Extension/InterfaceController.swift
@@ -49,13 +49,22 @@ class InterfaceController: WKInterfaceController {
     func randomizeTheNumber() {
         result = Int(arc4random_uniform(UInt32(diceNumberArray[diceSelect])))
         
+        if result == 0 {
+            
+            result = Int(arc4random_uniform(UInt32(diceNumberArray[diceSelect])))
+            
+            if result == 0 {
+                result = 1
+            }
+        }
+        
         printResult()
     }
     
     func increaseDiceArrayNumber()  {
         diceSelect += 1
         
-        if diceSelect >= diceNumberArray.count - 1 {
+        if diceSelect >= diceNumberArray.count {
             diceSelect = 0
         }
         
