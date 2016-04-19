d673b9b1d85b022f2a3f9929826084af3d0c82fb
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-19 09:36:40 +0300

    Lecture-73
    Hooking up the swift file

--- Section-8/Baby Name Generator/Baby Name Generator WatchKit Extension/InterfaceController.swift
+++ Section-8/Baby Name Generator/Baby Name Generator WatchKit Extension/InterfaceController.swift
@@ -12,20 +12,33 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    
+    @IBOutlet var lblName: WKInterfaceLabel!
+    @IBOutlet var btnBoyOrGirl: WKInterfaceButton!
+    
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
-        // Configure interface objects here.
     }
+    
 
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
+    
 
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
 
+    @IBAction func btnAnotherNameClick() {
+        
+    }
+    
+    
+    @IBAction func btnGirlOrBoyClick() {
+        
+    }
+    
 }
