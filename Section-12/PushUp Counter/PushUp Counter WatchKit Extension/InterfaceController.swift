1b0bb6ca66d5900212e177a1f3bb5abf790c3ed2
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-19 17:15:44 +0300

    Lecture-109
    Hooking up the Swift File

--- Section-12/PushUp Counter/PushUp Counter WatchKit Extension/InterfaceController.swift
+++ Section-12/PushUp Counter/PushUp Counter WatchKit Extension/InterfaceController.swift
@@ -11,21 +11,27 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    
+    @IBOutlet var btnReps: WKInterfaceButton!
+    @IBOutlet var btnMax: WKInterfaceButton!
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
-        // Configure interface objects here.
     }
 
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
 
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
 
+    @IBAction func btnRepsClick() {
+        
+    }
+    
+    @IBAction func btnMaxClick() {
+        
+    }
 }
