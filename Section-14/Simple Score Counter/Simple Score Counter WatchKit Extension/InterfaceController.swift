ca54c53a7daa8cf1e9b922ea79ddeeba9c45da5f
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-20 10:44:12 +0300

    Lecture-122
    Hooking up the Swift file

--- Section-14/Simple Score Counter/Simple Score Counter WatchKit Extension/InterfaceController.swift
+++ Section-14/Simple Score Counter/Simple Score Counter WatchKit Extension/InterfaceController.swift
@@ -12,20 +12,27 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var btnScore0: WKInterfaceButton!
+    @IBOutlet var btnScore1: WKInterfaceButton!
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
-        // Configure interface objects here.
     }
 
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
 
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
 
+    @IBAction func btnScore0Action() {
+    }
+    
+    @IBAction func btnScore1Action() {
+    }
+    
+    @IBAction func btnResetAction() {
+    }
 }
