c18d2c1836d90bfdaea3b7b4861f58e37d37482b
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-20 10:38:54 +0300

    Lecture-120
    Making a simple score counter

---
+++ Section-14/Simple Score Counter/Simple Score CounterTests/Simple_Score_CounterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Simple_Score_CounterTests.swift
+//  Simple Score CounterTests
+//
+//  Created by Pavlo Cretsu on 4/20/16.
+//  Copyright © 2016 Pasha Cretsu. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_Score_Counter
+
+class Simple_Score_CounterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
