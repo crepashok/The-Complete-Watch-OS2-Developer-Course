2734a7ab887105960a2052b8bdfe0da14295f466
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-20 12:36:26 +0300

    Lecture-131
    Adding in the variables

--- Section-15/Roman Numeral/Roman Numeral WatchKit Extension/InterfaceController.swift
+++ Section-15/Roman Numeral/Roman Numeral WatchKit Extension/InterfaceController.swift
@@ -13,8 +13,14 @@ import Foundation
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet var lblRomanNumber: WKInterfaceLabel!
+    @IBOutlet var lblCorrect: WKInterfaceLabel!
     @IBOutlet var btnAnswer0: WKInterfaceButton!
     @IBOutlet var btnAnswer1: WKInterfaceButton!
+    
+    var buttonCorrect : Int = 0
+    var totalCorrect : Int = 0
+    var romanNumeralArray = ["I", "V", "X", "L", "C", "D", "M"]
+    var randomArrayNumber : Int = 0
     
     
     override func awakeWithContext(context: AnyObject?) {
