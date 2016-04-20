f3b3b3cf1f0b6723929a77bf1fb33c5ae40dccbd
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-20 10:26:00 +0300

    Lecture-116
    Hooking up the Swift file

--- Section-13/Producitivity Timer/Producitivity Timer WatchKit Extension/InterfaceController.swift
+++ Section-13/Producitivity Timer/Producitivity Timer WatchKit Extension/InterfaceController.swift
@@ -12,20 +12,27 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var tmrTimer: WKInterfaceTimer!
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
-        // Configure interface objects here.
     }
 
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
 
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
 
+    @IBAction func btnStartClick() {
+        
+    }
+    
+    
+    @IBAction func btnResetClick() {
+        
+    }
+    
 }
